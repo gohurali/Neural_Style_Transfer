5bcf225e0513d2a91bb4d35e0676f892d5a4f6d5
gohurali <gohurali@live.com>
2019-04-20 00:44:28 -0700

    working on script version

--- train.py
+++ train.py
@@ -20,14 +20,35 @@ https://www.cv-foundation.org/openaccess/content_cvpr_2016/papers/Gatys_Image_St
 
 Using VGG19 Architecture for Feature Extraction
 """
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 class Trainer:
     def __init__(self):
         self.cfg = yaml.safe_load(open('config.yaml'))
+        self.content_im_loc = self.cfg['content_image_location']
+        self.style_im_loc = self.cfg['style_image_location']
+        self.img_size = self.cfg['image_size']
+        
+    
+    def preprocess_images(self, image, IMAGE_SIZE):
+        """Pre-processing images to the specifications
+        of the VGG19 Pre-Trained Model.
+        """
+        gen_transformer = transforms.Compose([
+            transforms.Resize(IMAGE_SIZE),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406],[0.229, 0.224, 0.225])
+        ])
+        return gen_transformer(image).to(device)
+    
+    def get_mode(self):
         pass
+    
 
 
 def main():
-    pass
+    t = Trainer()
 
 if __name__=='__main__':
     main()
